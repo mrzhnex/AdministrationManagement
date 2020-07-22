a4aa6457df0b62e8b9f83d92e63e2580e75441f2
Tabula <MrZhnex@ya.ru>
2020-07-22 15:16:33 +0300

    Upload

---
+++ MainSettings.cs
@@ -0,0 +1,21 @@
+﻿using EXILED;
+
+namespace AdministrationManagement
+{
+    public class MainSettings : Plugin
+    {
+        public override string getName => nameof(AdministrationManagement);
+
+        public override void OnEnable()
+        {
+
+        }
+
+        public override void OnDisable()
+        {
+
+        }
+
+        public override void OnReload() { }
+    }
+}
